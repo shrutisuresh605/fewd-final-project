cee1c033dcbf5c756036ac7ce7f67b6db8f0015f
shrutisuresh605 <shrutisuresh605@gmail.com>
2020-06-21 21:12:09 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef*/
+
+jQuery(document).ready(function() {
+  $(window).scroll(function(e) {
+    parallaxScroll();
+  });
+
+  function parallaxScroll() {
+    var scrolled = $(window).scrollTop();
+
+    $("#parallax-bg-1").css("top", 0 - scrolled * 0.25 + "px");
+    $("#parallax-bg-2").css("top", 0 - scrolled * 0.4 + "px");
+    $("#parallax-bg-3").css("top", 0 - scrolled * 0.7 + "px");
+    $("#parallax-bg-4").css("top", 0 - scrolled * 0.2 + "px");
+    $("#parallax-bg-5").css("top", 0 - scrolled * 0.4 + "px");
+  }
+});
+
+const hamburgerMenu = document.querySelector(".hamburger");
+const sideNav = document.querySelector(".nav-links");
+const navLink = document.querySelectorAll("span");
+
+hamburgerMenu.addEventListener("click", function() {
+  sideNav.classList.toggle("side-nav");
+  for (var i = 0; i < navLink.length; i++) {
+    navLink[i].classList.toggle("hamburger-bar");
+    navLink[i].classList.toggle("hamburger-bar-" + [i]);
+  }
+});
